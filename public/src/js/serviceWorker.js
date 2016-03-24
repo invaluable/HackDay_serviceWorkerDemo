d8385b117e0611ecbdc39987bc1f6d83415c4a31
Jeremy <jball@invaluable.com>
2016-03-24 14:56:56 -0400

    more cleanup

--- public/src/js/serviceWorker.js
+++ public/src/js/serviceWorker.js
@@ -2,13 +2,9 @@ export function startWorker() {
 
   if('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register('sw.js', { scope: './'})
-      .then(function(registration) {
-        console.log('Service worker registered!');
-      })
-      .catch(function(error) {
-        console.log('There was an error!');
-      });
+      .register( 'sw.js', { scope: './'} )
+      .then( registration => console.log('Service worker registered!') )
+      .catch( error => console.log('There was an error!') );
   }
   else {
     console.log('service workers not supported');
